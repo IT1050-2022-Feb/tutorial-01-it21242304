8344242095dc43bdb05b2e18b79df15408e55b7f
it21242304 <100022710+it21242304@users.noreply.github.com>
2022-02-20 10:14:56 +0800

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -17,3 +17,26 @@ int main() {
    printf("%d ", multiply(no1, no2));
    return 0;
 }
+int maximum(int no1, int no2)
+{
+   int max=0;
+   if(no1 > no2)
+      max=no1;
+   if(no1 < no2)
+      max=no2;
+   
+  return max;
+}
+int minimum(int no1,int no2)
+{
+   int min=0;
+   if(no1 > no2)
+      min=no2;
+   if(no1< no2)
+      min=no1;
+   return min;
+   
+   int multiply(int no1,int no2)
+   {
+      return no1*no2;
+   }
